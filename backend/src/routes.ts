a6122023c9259cc9d67d6f0699626ea702058889
Murilo Prado <kruzprado@hotmail.com>
2020-06-02 23:10:57 -0300

    :tada: first commit

---
+++ backend/src/routes.ts
@@ -0,0 +1,18 @@
+import express from "express";
+
+import ItemsController from "./controllers/ItemsController";
+import PointsController from "./controllers/PointsController";
+
+const routes = express.Router();
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
+
+// index, show, create, update, delete
+
+routes.get("/items", itemsController.index);
+
+routes.post("/points", pointsController.create);
+routes.get("/points/:id", pointsController.show);
+routes.get("/points", pointsController.index);
+
+export default routes;
